50b3debc0587beeee0647579de134b0fee96f143
tanaka2724 <h.tanaka.24.4.1317@4nonome.com>
2024-05-02 09:54:43 +0900

    制御構文の解答

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,29 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+a = int(input("aの値を入力: "))
+b = int(input("bの値を入力: "))
 
 # TODO
+
+#aの素数判定
+factor = 2
+
+while factor * factor <= a:
+    if a % factor == 0:
+        print(str(a),"は素数ではない")
+        break
+    
+    else:
+        factor += 1
+else:
+     print(str(a),"は素数です")       
+     
+     
+#bの素数判定
+factor = 2
+while factor * factor <= b:
+    if b % factor == 0:
+        print(str(b), "は素数ではない")
+        break
+    else:
+        factor += 1
+else:
+    print(str(b), "は素数です")     
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,21 @@
+import math
 from math import sin
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
+
+def f(x):
+    return sin(x)
+
+N = 100
+a = 0
+b = math.pi / 2
+
+h = (b - a) / N
+
+S = 0
+for k in range(1,N):
+    S += (h / 2) * (f(a + (k - 1) * h) + f(a + k * h))
+
+print(S) 
