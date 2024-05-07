edfee876b484893d21f973ef8c894466ea618b37
tanaka2724 <h.tanaka.24.4.1317@4nonome.com>
2024-05-07 09:41:34 +0900

    制御関数修正

--- prime_number.py
+++ prime_number.py
@@ -2,11 +2,9 @@ a = int(input("aの値を入力: "))
 b = int(input("bの値を入力: "))
 
 # TODO
-
 #aの素数判定
 factor = 2
-
-while factor * factor <= a:
+while factor * factor <= a :
     if a % factor == 0:
         print(str(a),"は素数ではない")
         break
@@ -14,7 +12,10 @@ while factor * factor <= a:
     else:
         factor += 1
 else:
-     print(str(a),"は素数です")       
+    if a == 1:
+        print(str(a), "は素数ではない")
+    else:
+        print(str(a),"は素数です")       
      
      
 #bの素数判定
@@ -26,4 +27,12 @@ while factor * factor <= b:
     else:
         factor += 1
 else:
-    print(str(b), "は素数です")     
+    if b == 1:
+        print(str(b),"は素数ではない")
+    else:
+        print(str(b), "は素数です")     
+
+
+
+    
+    
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -15,7 +15,9 @@ b = math.pi / 2
 h = (b - a) / N
 
 S = 0
-for k in range(1,N):
+for k in range(1,N + 1):
     S += (h / 2) * (f(a + (k - 1) * h) + f(a + k * h))
+    
+    print(k)
 
 print(S) 
